3d4ccfcc54e6fdf28de72fe4ce3e9d361cb975c6
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-03-25 15:33:36 +0200

    First commit

---
+++ Lab_3/DFS_and_BFS.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def dfs_util(self, v, visited):
+        visited.add(v)
+        print(v, end=' ')
+
+        for neighbor in self.graph[v]:
+            if neighbor not in visited:
+                self.dfs_util(neighbor, visited)
+
+    def dfs(self, start):
+        visited = set()
+        self.dfs_util(start, visited)
+
+    def bfs(self, start):
+        visited = set()
+        queue = [start]
+        visited.add(start)
+
+        while queue:
+            v = queue.pop(0)
+            print(v, end=' ')
+
+            for neighbor in self.graph[v]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+
+# Example Usage:
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("DFS traversal starting from vertex 2:")
+g.dfs(2)
+print("\nBFS traversal starting from vertex 2:")
+g.bfs(2)
